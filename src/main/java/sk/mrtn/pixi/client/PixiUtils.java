700c8e49981b0c90097608338fcca98946e793eb
Martin Liptak <martin.liptak@sowhat.sk>
2016-08-29 23:28:15 +0200

    removed common native cast for EmitterConfig within PixiUtils

--- src/main/java/sk/mrtn/pixi/client/PixiUtils.java
+++ src/main/java/sk/mrtn/pixi/client/PixiUtils.java
@@ -1,16 +1,9 @@
 package sk.mrtn.pixi.client;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import sk.mrtn.pixi.client.particles.config.EmitterConfig;
-
 /**
  * Created by klaun on 23/08/16.
  * TODO: Tomi konzultacia
  */
 public class PixiUtils {
 
-    // at the time I did not know how to cast extept this cheating :-(
-    public static native EmitterConfig castToEmitter(JavaScriptObject jConfig) /*-{
-        return jConfig;
-    }-*/;
 }
